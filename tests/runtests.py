26e3c2ff3b6383139174beb95fe5c60d2bfb3514
Lukas Puehringer <lukas.puehringer@nyu.edu>
2018-02-01 11:39:18 -0500

    Where's in-toto?

--- tests/runtests.py
+++ tests/runtests.py
@@ -17,15 +17,20 @@
   Script to search, load and run in-toto tests using the Python `unittest`
   framework.
 """
+from __future__ import print_function
 import sys
 import logging
 from unittest import defaultTestLoader, TextTestRunner
 
+
+
 import in_toto.log
 
-# Override in-toto logger default StreamHandler to prevent test log inundation
-in_toto.log.logger.handlers = [logging.NullHandler()]
+print(in_toto.log.__file__)
 
-suite = defaultTestLoader.discover(start_dir=".")
-result = TextTestRunner(verbosity=2, buffer=True).run(suite)
-sys.exit(0 if result.wasSuccessful() else 1)
+# Override in-toto logger default StreamHandler to prevent test log inundation
+# in_toto.log.logger.handlers = [logging.NullHandler()]
+
+# suite = defaultTestLoader.discover(start_dir=".", pattern="test_log.py")
+# result = TextTestRunner(verbosity=1, buffer=True).run(suite)
+# sys.exit(0 if result.wasSuccessful() else 1)
